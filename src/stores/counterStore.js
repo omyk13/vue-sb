db2de5e96b013ab82db9098937c8aa623c575c0d
omyk13 <onkar.kadam07@gmail.com>
2026-01-18 15:05:39 -0500

    added some logic in counterstore for ramp

--- src/stores/counterStore.js
+++ src/stores/counterStore.js
@@ -1,9 +1,19 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { useCounter } from '../composables/useCounter.js'
+import { useSettingStore } from './settingStore.js'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const settings = useSettingStore()
+
+  function increment(){
+    count.value += settings.step
+  }
+
+  function decrement(){
+    count.value -= settings.step
+  }
 
   const { doubled, canDecrement, increment, decrement, reset } = useCounter(count)
 
